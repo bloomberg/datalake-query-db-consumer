f793118cd242aa547a8cdf69014de7cfa7b57039
Cristian Osiac <mosiac@bloomberg.net>
2022-05-05 17:59:45 +0100

    Support more columns and tables for Trino 379
    Add `processedInputBytes` and `processedInputRows` columns added in Trino 378/377.
    Add `VARCHAR(100) uri`, `TEXT plan`, `JSON payload`, `JSON planNodeStatsAndCosts` and `JSON sessionProperties` columns for storing existing data that wasn't captured before.
    
    Add `client_tag`, `resource_groups`, and `operator_summaries` tables for storing existing data that wasn't captured.
    
    Add test for new columns and tables.

--- tests/integration/test_consumer.py
+++ tests/integration/test_consumer.py
@@ -27,7 +27,13 @@ from confluent_kafka import KafkaException
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm.session import Session, sessionmaker
 
-from bloomberg.datalake.datalakequerydbconsumer._data_models import ColumnMetrics, QueryMetrics
+from bloomberg.datalake.datalakequerydbconsumer._data_models import (
+    ClientTags,
+    ColumnMetrics,
+    OperatorSummaries,
+    QueryMetrics,
+    ResourceGroups,
+)
 from bloomberg.datalake.datalakequerydbconsumer._kafka import KafkaConsumer
 
 from .._utils import get_raw_metrics
@@ -95,6 +101,9 @@ def _cleanup(session: Session):
 
     yield
 
+    session.query(ClientTags).delete()
+    session.query(ResourceGroups).delete()
+    session.query(OperatorSummaries).delete()
     session.query(ColumnMetrics).delete()
     session.query(QueryMetrics).delete()
     session.commit()
@@ -129,23 +138,33 @@ def test_consumer(producer, consumer_queue: Queue, session):
     assert result.queryId == _query_id
     assert result.transactionId == "3e7820f0-4a9e-498f-88d8-c206ac85bd3e"
     assert result.query == "SELECT * FROM table;"
+    assert result.uri == "http://localhost:8080/v1/query/20210720_093342_00012_pzafy"
+    assert "Fragment 0" in result.plan
+    assert "└─" in result.plan  # Ensure unicode characters are persisted correctly
+    assert result.payload["stageId"] == "20220505_100804_00003_7dpwd.0"
     assert result.queryType == "SELECT"
     assert result.remoteClientAddress == "127.0.0.1"
     assert result.user == "test-user"
     assert result.userAgent == "StatementClientV1/8012395-dirty"
     assert result.source == "datalake-cli"
     assert result.serverAddress == "localhost"
-    assert result.serverVersion == "dev"
+    assert result.serverVersion == "379"
     assert result.environment == "test"
+    assert result.sessionProperties == {"catalog": "postgresql"}
     assert result.cpuTime == 25.975
+    assert result.failedCpuTime == 0
     assert result.wallTime == 112.444
     assert result.queuedTime == 0.001
     assert result.scheduledTime == 156.696
+    assert result.failedScheduledTime == 1
     assert result.analysisTime == 12.178
     assert result.planningTime == 1.733
     assert result.executionTime == 100.266
+    assert result.inputBlockedTime == 7.72
+    assert result.failedInputBlockedTime == 0.1
+    assert result.outputBlockedTime == 0.2
+    assert result.failedOutputBlockedTime == 0.3
     assert result.peakUserMemoryBytes == 24610191
-    assert result.peakTotalNonRevocableMemoryBytes == 58178441
     assert result.peakTaskUserMemory == 19472880
     assert result.peakTaskTotalMemory == 43828971
     assert result.physicalInputBytes == 0
@@ -158,9 +177,12 @@ def test_consumer(producer, consumer_queue: Queue, session):
     assert result.outputRows == 160908
     assert result.writtenBytes == 0
     assert result.writtenRows == 0
+    assert result.processedInputRows == 1
+    assert result.processedInputBytes == 571
     assert result.cumulativeMemory == 2143774060571.0
     assert result.completedSplits == 507
     assert result.resourceWaitingTime == 12.178
+    assert result.planNodeStatsAndCosts == {"stats": {}, "costs": {}}
     assert result.createTime == datetime.fromtimestamp(1626773622)
     assert result.executionStartTime == datetime.fromtimestamp(1626773634)
     assert result.endTime == datetime.fromtimestamp(1626773734)
@@ -200,6 +222,51 @@ def test_consumer(producer, consumer_queue: Queue, session):
     assert result[2].columnName == "test-column-2_1"
     assert result[2].physicalInputBytes == 0
     assert result[2].physicalInputRows == 2
+
+    result = session.query(ClientTags).order_by(ClientTags.clientTag.asc()).filter_by(queryId=_query_id).all()
+    assert len(result) == 2
+    client_tag_1, client_tag_2 = result
+    assert client_tag_1.queryId == _query_id
+    assert client_tag_1.clientTag == "load_balancer"
+    assert client_tag_2.queryId == _query_id
+    assert client_tag_2.clientTag == "superset"
+
+    result = (
+        session.query(ResourceGroups).order_by(ResourceGroups.resourceGroup.asc()).filter_by(queryId=_query_id).all()
+    )
+    assert len(result) == 2
+    resource_group_1, resource_group_2 = result
+    assert resource_group_1.queryId == _query_id
+    assert resource_group_1.resourceGroup == "admin"
+    assert resource_group_2.queryId == _query_id
+    assert resource_group_2.resourceGroup == "global"
+
+    result = (
+        session.query(OperatorSummaries)
+        .order_by(OperatorSummaries.operatorSummary["stageId"].as_integer().asc())
+        .order_by(OperatorSummaries.operatorSummary["pipelineId"].as_integer().asc())
+        .order_by(OperatorSummaries.operatorSummary["operatorId"].as_integer().asc())
+        .order_by(OperatorSummaries.operatorSummary["planNodeId"].as_integer().asc())
+        .filter_by(queryId=_query_id)
+        .all()
+    )
+    assert len(result) == 4
+    assert result[0].operatorSummary["stageId"] == 0
+    assert result[0].operatorSummary["pipelineId"] == 0
+    assert result[0].operatorSummary["operatorId"] == 0
+    assert result[0].operatorSummary["planNodeId"] == "89"
+    assert result[1].operatorSummary["stageId"] == 0
+    assert result[1].operatorSummary["pipelineId"] == 0
+    assert result[1].operatorSummary["operatorId"] == 1
+    assert result[1].operatorSummary["planNodeId"] == "6"
+    assert result[2].operatorSummary["stageId"] == 1
+    assert result[2].operatorSummary["pipelineId"] == 0
+    assert result[2].operatorSummary["operatorId"] == 0
+    assert result[2].operatorSummary["planNodeId"] == "0"
+    assert result[3].operatorSummary["stageId"] == 1
+    assert result[3].operatorSummary["pipelineId"] == 0
+    assert result[3].operatorSummary["operatorId"] == 1
+    assert result[3].operatorSummary["planNodeId"] == "0"
 
 
 def test_fail_to_handle_dosent_commit(producer, bad_handle_consumer):
